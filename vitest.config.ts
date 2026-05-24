b8a1c4f75d73696178a84d96f3251d43a4382302
kreatweb <oreoglaze07@gmail.com>
2026-05-24 13:48:15 +0530

    Update vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
+  base: '/herb-olive-garden/',
   plugins: [react()],
   test: {
     environment: "jsdom",
